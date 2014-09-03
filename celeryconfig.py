e29b2c48fcfceae5c003e404aa2d1d421cd03a17
Jason Boyles <jason@duncancreek.net>
2014-09-03 11:48:46 -0500

    update schedule

--- celeryconfig.py
+++ celeryconfig.py
@@ -7,7 +7,7 @@ CELERY_TIMEZONE = os.environ.get('TZ', 'UTC')
 CELERYBEAT_SCHEDULE = {
     'run-daily-builds': {
         'task': 'tasker.trigger_daily_builds',
-        'schedule': crontab(hour=16, minute=45, day_of_week='*'),
+        'schedule': crontab(hour=17, minute=10, day_of_week='*'),
         'args': ['rackspace-orchestration-templates']
     }
 }
