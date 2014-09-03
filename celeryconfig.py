74dc2568e5ebac76def4fdd2fe41ad563a81e73f
Jason Boyles <jason@duncancreek.net>
2014-09-03 11:41:45 -0500

    update DOW in cron config

--- celeryconfig.py
+++ celeryconfig.py
@@ -7,7 +7,7 @@ CELERY_TIMEZONE = os.environ.get('TZ', 'UTC')
 CELERYBEAT_SCHEDULE = {
     'run-daily-builds': {
         'task': 'tasker.trigger_daily_builds',
-        'schedule': crontab(hour=16, minute=36, day_of_week='wed'),
+        'schedule': crontab(hour=16, minute=45, day_of_week='*'),
         'args': ['rackspace-orchestration-templates']
     }
 }
