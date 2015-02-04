2d8a1e55ec3e3d3a9956541f9c016b6682c4b965
Jason Boyles <jason@duncancreek.net>
2015-02-04 10:07:09 -0600

    add rackspace-cookbooks build schedule

--- celeryconfig.py
+++ celeryconfig.py
@@ -5,7 +5,7 @@ BROKER_URL = 'redis://redis:6379/0'
 
 CELERY_TIMEZONE = os.environ.get('TZ', 'America/Chicago')
 CELERYBEAT_SCHEDULE = {
-    'run-daily-builds': {
+    'run-daily-builds-heat': {
         'task': 'tasker.trigger_daily_builds',
         'schedule': crontab(hour=4, minute=13),
         'args': ['rackspace-orchestration-templates']
@@ -14,5 +14,10 @@ CELERYBEAT_SCHEDULE = {
         'task': 'tasker.trigger_failed_builds',
         'schedule': crontab(hour=6, minute=13),
         'args': ['rackspace-orchestration-templates']
+    },
+    'run-daily-builds-rackspace-cookbooks': {
+        'task': 'tasker.trigger_daily_builds',
+        'schedule': crontab(minute=15),
+        'args': ['rackspace-cookbooks']
     }
 }
